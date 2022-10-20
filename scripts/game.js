63b0de34dda031d8ac3406bb35148ae5ba25ac33
Matheus Tonon <math.tonon@gmail.com>
2022-10-20 07:55:13 -0300

    creating the logic for checking winner or draw

--- scripts/game.js
+++ scripts/game.js
@@ -27,16 +27,66 @@ function selectGameField(event) {
   const selectedRow = selectedField.dataset.row - 1;
 
   if (gameData[selectedRow][selectedColumn] > 0) {
-    alert('Please select an empty field!')
+    alert('Please select an empty field!');
     return;
   }
 
   selectedField.textContent = players[activePlayer].symbol;
   selectedField.classList.add('disabled');
 
-
   gameData[selectedRow][selectedColumn] = activePlayer + 1;
   console.log(gameData);
 
+  const winnerId = checkForGameOver();
+  console.log(winnerId);
+
+  currentRound++;
   switchPlayer();
+}
+
+function checkForGameOver() {
+  //checking the rows for equality
+  for (let i = 0; i < 3; i++) {
+    if (
+      gameData[i][0] > 0 &&
+      gameData[i][0] === gameData[i][1] &&
+      gameData[i][1] === gameData[i][2]
+    ) {
+      return gameData[i][0];
+    }
+  }
+
+  // checking the columns for equality
+  for (let i = 0; i < 3; i++) {
+    if (
+      gameData[0][i] > 0 &&
+      gameData[0][i] === gameData[1][i] &&
+      gameData[0][i] === gameData[2][i]
+    ) {
+      return gameData[0][i];
+    }
+  }
+
+  // Diagonal: top left to bottom right
+  if (
+    gameData[0][0] > 0 &&
+    gameData[0][0] === gameData[1][1] &&
+    gameData[1][1] === gameData[2][2]
+  ) {
+    return gameData[0][0];
+  }
+
+  // Diagonal: bottom left to top right
+  if (
+    gameData[2][0] > 0 &&
+    gameData[2][0] === gameData[1][1] &&
+    gameData[1][1] === gameData[0][2]
+  ) {
+    return gameData[2][0];
+  }
+
+  if (currentRound === 9) {
+    return -1;
+  }
+  return 0;
 }
