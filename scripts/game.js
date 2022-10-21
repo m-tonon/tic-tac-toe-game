8083d4e52afb14b919bde1aeffac76c54af737f4
Matheus Tonon <math.tonon@gmail.com>
2022-10-21 06:21:23 -0300

    created ending game & restart logic

--- scripts/game.js
+++ scripts/game.js
@@ -1,8 +1,30 @@
+function resetGameStatus() {
+  activePlayer = 0;
+  currentRound = 0;
+  gameIsOver = false;
+  gameOverElement.firstElementChild.innerHTML =
+    'You won, <span id="winner-name">PLAYER NAME</span>!';
+  gameOverElement.style.display = 'none';
+
+  let gameBoardIndex = 0;
+  for (let i = 0; i < 3; i++) { // To reset gameData
+    for (let j = 0; j < 3; j++) {
+      gameData[i][j] = 0;
+      const gameBoardItemElement = gameBoardElement.children[gameBoardIndex];
+      gameBoardItemElement.textContent = '';
+      gameBoardItemElement.classList.remove('disabled');
+      gameBoardIndex++;
+    }
+  }
+}
+
 function startNewGame() {
   if (players[0].name === '' || players[1].name === '') {
     alert('Please set custom player names for both players!');
     return;
   }
+
+  resetGameStatus();
 
   activePlayerNameElement.textContent = players[activePlayer].name;
   gameAreaElement.style.display = 'block';
@@ -18,7 +40,8 @@ function switchPlayer() {
 }
 
 function selectGameField(event) {
-  if (event.target.tagName !== 'LI') {
+  if (event.target.tagName !== 'LI' || gameIsOver) {
+    //Prevent to click outside the 'li box'
     return;
   }
 
@@ -34,11 +57,14 @@ function selectGameField(event) {
   selectedField.textContent = players[activePlayer].symbol;
   selectedField.classList.add('disabled');
 
-  gameData[selectedRow][selectedColumn] = activePlayer + 1;
+  gameData[selectedRow][selectedColumn] = activePlayer + 1; // Store which player selected that box
   console.log(gameData);
 
   const winnerId = checkForGameOver();
-  console.log(winnerId);
+
+  if (winnerId !== 0) {
+    endGame(winnerId);
+  }
 
   currentRound++;
   switchPlayer();
@@ -89,4 +115,17 @@ function checkForGameOver() {
     return -1;
   }
   return 0;
+}
+
+function endGame(winnerId) {
+  gameIsOver = true;
+  gameOverElement.style.display = 'block';
+
+  if (winnerId > 0) {
+    const winnerName = players[winnerId - 1].name;
+    gameOverElement.firstElementChild.firstElementChild.textContent =
+      winnerName;
+  } else {
+    gameOverElement.firstElementChild.textContent = "It's a draw!";
+  }
 }
